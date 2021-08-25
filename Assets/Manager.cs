03f73b670829fa53249a4e0d7af43248857ba34f
Jason_Teng <50354880+sdonline13@users.noreply.github.com>
2021-08-25 10:28:56 +0800

    實現外部導入 物件

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -7,11 +7,14 @@ using UnityEngine.UI;
 
 //https://www.youtube.com/watch?v=0tFnqBdO7NY
 //https://z0935323866.medium.com/unity-%E5%B0%8B%E5%9D%80%E5%BC%8F%E8%B3%87%E6%BA%90%E7%AE%A1%E7%90%86%E7%B3%BB%E7%B5%B1addressable-assets-system-%E4%B8%80-bb1e99014a88
+//https://z0935323866.medium.com/unity-%E5%B0%8B%E5%9D%80%E5%BC%8F%E8%B3%87%E6%BA%90%E7%AE%A1%E7%90%86%E7%B3%BB%E7%B5%B1addressable-assets-system-%E4%BA%8C-eb0e302fd4db
 //https://zhuanlan.zhihu.com/p/94572467
 /*
 BuildTarget : 要建立的平台位置，這裡基本上不用到到，它會自動根據我們在BuildSetting設定的平台建立相對應的資料夾。
 LocalBuildPath、LocalLoadPath : 在本地端要儲存與載入的路徑。
 RemoteBuildPath、RemoteLoadPath : 在遠端儲存與載入的路徑。
+
+使用遠端測試 :PlayModfeScript -> Use Existing Build ,遠端模式必須要先build出一個bundle檔案出來才能測試。
  */
 
 public class Manager : MonoBehaviour
@@ -39,9 +42,9 @@ public class Manager : MonoBehaviour
         //assetobject.LoadAssetAsync<GameObject>().Completed += OnAssetObjLoaded;//當前資源載完執行 
 
         //AssetLabelReference 方式
-        //Addressables.LoadAssetAsync<GameObject>(assetLabelobject).Completed += OnAssetObjLoaded;
+        Addressables.LoadAssetAsync<GameObject>(assetLabelobject).Completed += OnAssetObjLoaded;
         //使用 async
-        AsyncInstanctiate();
+        //AsyncInstanctiate();
         LoadObjBtn.onClick.AddListener(CreateObjBtn);
     }
 
@@ -67,6 +70,9 @@ public class Manager : MonoBehaviour
         //assetobject.Instantiate(pos, Quaternion.identity);//AssetReference內部加載
          
     }
+    /// <summary>
+    /// 使用 async task家載
+    /// </summary>
     async void AsyncInstanctiate()
     {
         assetObj = await assetobject.LoadAssetAsync<GameObject>().Task ;
